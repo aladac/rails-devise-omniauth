e22770c53d10fea8d05589c37430686364b1ff59
Adam Ladachowski <ladachowski@cic.com>
2019-07-14 20:40:09 +0200

    cool

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[google_oauth2 facebook twitter github]
+  devise :rememberable, :omniauthable, omniauth_providers: %i[google_oauth2 facebook twitter github]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email || "#{SecureRandom.hex}@#{SecureRandom.hex}"
-      user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.image = auth.info.image
     end
